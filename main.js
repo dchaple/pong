8a3dae1c884bf4cddbf5d1f2ac38ddd43d0cd469
dchaple23 <79047152+dchaple@users.noreply.github.com>
2021-03-22 21:33:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import Phaser from 'phaser' 
+
+import TitleScreen from './scenes/TitleScreen'
+
+const config = {
+    width: 800,
+    height: 500,
+    type: Phaser.AUTO
+}
+
+const game = new Phaser.Game(config) 
+
+game.scene.add('titlescreen', TitleScreen)
+
+game.scene.start('titlescreen')
+
